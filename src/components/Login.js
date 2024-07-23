8e95ada5e94a298abfcb351255cb651a7ec8ad9b
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-07-23 12:13:08 +1200

    fixing the code after review

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,57 @@
 import React from 'react';
+import { baseUrl } from '../constant';
 import axios from 'axios';
 import { useState } from 'react';
 
 function Login(props) {
-    const [a, setA] = useState("");
-    const [b, setB] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const [login_status, setLogin_Status] = useState("");
 
-    function aHandler(e) {
-        setA(e.target.value);
+    function userNameHandler(e) {
+        setUsername(e.target.value);
     }
 
-    function bHandler(e) {
-        setB(e.target.value);
+    function passwordHandler(e) {
+        setPassword(e.target.value);
     }
 
-    function testing1()
+    function login()
     {
         let data = JSON.stringify({
-            "username": "john",
-            "password": "123"
+            "username": username,
+            "password": password
           });
           
-                let config = {
-                    method: 'post',
-                            maxBodyLength: Infinity,
-                                url: 'https://message-project-nine.vercel.app/auth/',
-                                headers: { 
-                             'Content-Type': 'application/json', 
-                        },
-                        data : data
-                };
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: baseUrl+'auth/',
+            headers: { 
+                'Content-Type': 'application/json', 
+            },
+            data : data
+        };
         
-            axios.request(config)
-                .then((response) => {
-                    console.log(JSON.stringify(response.data));setLogin_Status("Login Success!");localStorage.setItem('authentication', response.data.token);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    setLogin_Status("Username or password is wrong!")
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+                setLogin_Status("Login Success!");
+                localStorage.setItem('authentication', response.data.token);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLogin_Status("Username or password is wrong!")
 
-                });
+            });
     }
 
     return (
         <div>
             <h1>Login Page</h1>
-                    <p>Username: <input id={"username"} type={'text'} onChange={aHandler}/></p>
-            <p>Password: <input id={"password"} type={'password'} onChange={bHandler} /></p>
-                    <p><button id={"loginbtn"} onClick={testing1}>Login</button></p>
+            <p>Username: <input id={"username"} type={'text'} onChange={userNameHandler}/></p>
+            <p>Password: <input id={"password"} type={'password'} onChange={passwordHandler} /></p>
+            <p><button id={"loginbtn"} onClick={login}>Login</button></p>
             <p id={'login_status'}>{login_status}</p>
         </div>
     );
